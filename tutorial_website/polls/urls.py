180a2f0b76cc7a9912bda1039b5b6ac711d1bcaa
AbhigyanBose <abhigyandeepbose@gmail.com>
2022-05-14 00:53:02 +0530

    Added Models and basic read operation.

--- tutorial_website/polls/urls.py
+++ tutorial_website/polls/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('polls/question/<int:question_id>', views.question, name='question'),
+    path('polls/choice/<int:choice_id>', views.choice, name='choice')
 ]
--- tutorial_website/polls/views.py
+++ tutorial_website/polls/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question, Choice
+from django.http import HttpResponse, Http404
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Polls Index</h1>")
+    questions = Question.objects.all()
+    return render(request, 'polls/index.html', context={'items': questions})
+
+
+def question(request, question_id):
+    choices = Choice.objects.filter(question_id=question_id)
+    if not choices:
+        get_object_or_404(Question, pk=question_id)
+        raise Http404("Question has no choices")
+
+    context = {'choices': choices}
+    return render(request, 'polls/question.html', context=context)
+
+
+def choice(request, choice_id):
+    return HttpResponse("Choice Selected "+str(choice_id))
